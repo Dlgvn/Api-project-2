6b6f4a43aafa9fdbcce068399ff79f03a8888285
Dlgvn <u.dulguun@letumongolia.com>
2025-04-14 13:07:41 +0800

    main.py

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
-from typing import Optional
-
 from fastapi import FastAPI
+from admin import router as admin_router
+from user import router as user_router
 
 app = FastAPI()
 
+app.include_router(admin_router)
+app.include_router(user_router)
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
